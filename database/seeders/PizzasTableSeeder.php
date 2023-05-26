3d857a23b4fe1a866b84d1c99f584b657b7147e2
ClaudioLeone <claleone92@gmail.com>
2023-05-26 17:00:52 +0200

    Created seeders

--- database/seeders/PizzasTableSeeder.php
+++ database/seeders/PizzasTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Pizza;
+use Faker\Generator as Faker;
 
 class PizzasTableSeeder extends Seeder
 {
@@ -12,8 +14,15 @@ class PizzasTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 20; $i++) { 
+            $pizza = new Pizza();
+            $pizza->name = $faker->randomElement(['Mimmo', 'Margherita', 'Pippo', 'Pluto', 'Paperino']);
+            $pizza->description = $faker->randomElement(['Mediocre', 'Buona', 'Perfetta']);
+            $pizza->price = $faker->randomElement([4, 9, 20, 7, 11]);
+            $pizza->vegetarian = $faker->randomElement([true, false]);
+            $pizza->save();
+        }
     }
 }
