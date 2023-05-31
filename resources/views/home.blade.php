54a47902f73ad0f3180883f54f9b1f21ad461575
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-05-31 15:36:27 +0200

    Add:scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container text-center mt-4 homepage">
-        <h1 text>HOMEPAGE PIZZAS</h1>
-        <a href="{{ route('pizzas.index') }}">GO TO PIZZAS</a>
+    <div class="container homepage text-center  d-flex align-items-center justify-content-center">
+        <div class="homepage_title rounded-circle  ">
+            <h1 class="title">ITALIAN PIZZA'S </h1>
+            <a class="btn btn-outline-success" href="{{ route('pizzas.index') }}">Menu</a>
+        </div>
+       
     </div>
 @endsection
