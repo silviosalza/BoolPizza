1a7563ef3fb8706426c5664cb31c3f9b0e9e3191
silviosalza <silviofilipposalza@gmail.com>
2023-06-12 15:57:25 +0200

    seeder added

--- database/seeders/IngredientSeeder.php
+++ database/seeders/IngredientSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Ingredient;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class IngredientSeeder extends Seeder
 {
@@ -14,6 +16,13 @@ class IngredientSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $ingredients = ['Pomodoro', 'Mozzarella', 'Salame', 'Ananas', 'Gatto'];
+        foreach ($ingredients as $ingredient_value) {
+            # code...
+            $new_type = new Ingredient();
+            $new_type->name = $ingredient_value;
+            $new_type->slug = Str::slug($ingredient_value);
+            $new_type->save();
+        }
     }
 }
