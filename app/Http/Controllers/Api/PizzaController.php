5bdf5ee44c49b4ac4e4689e00f35de5476b34cab
diegoG82 <diegogiardina@hotmail.it>
2023-06-19 15:43:21 +0200

    done milestone 1

--- app/Http/Controllers/Api/PizzaController.php
+++ app/Http/Controllers/Api/PizzaController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Pizza;
 use Illuminate\Http\Request;
+
 
 class PizzaController extends Controller
 {
-    //
+    public function index () {
+        $pizzas = Pizza::all();
+        return response () ->Json([
+        'success' => true,
+        'results' => $pizzas
+        ]);
+}
 }
